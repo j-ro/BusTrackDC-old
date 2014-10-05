bbd8fe4b53a74f8510ded28afbef0f56c0d69685
Jason Rosenbaum <seminal@theseminal.com>
2014-10-05 14:21:33 -0400

    new blank app

---
+++ bustrackdc/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/AndroidManifest.xml \
---
+++ bustrackdc/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes.dex : \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/BuildConfig.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/BusTrackDC.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/R$attr.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/R$drawable.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/R$string.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/R$xml.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/classes/com/jasonrosenbaum/bustrackdc/R.class \
+/Users/jasonrosenbaum/xcode/BusTrackDC/bustrackdc/platforms/android/ant-build/dexedLibs/classes-cb0f295c189573958e12ab4e89b1e4c7.jar \
