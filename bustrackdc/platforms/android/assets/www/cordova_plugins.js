c5a8620ddd5e06c9dae7db7f39f78f8c1b4fd4b5
Jason Rosenbaum <seminal@theseminal.com>
2014-10-05 13:29:41 -0400

    adding debug plugin

--- bustrackdc/platforms/android/assets/www/cordova_plugins.js
+++ bustrackdc/platforms/android/assets/www/cordova_plugins.js
@@ -12,7 +12,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.geolocation": "0.3.10"
+    "org.apache.cordova.geolocation": "0.3.10",
+    "org.apache.cordova.console": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
--- bustrackdc/platforms/ios/www/cordova_plugins.js
+++ bustrackdc/platforms/ios/www/cordova_plugins.js
@@ -34,13 +34,28 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.geolocation": "0.3.10"
+    "org.apache.cordova.geolocation": "0.3.10",
+    "org.apache.cordova.console": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
